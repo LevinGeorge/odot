26af4b15fb388eab6e53d4cc736d9e26c41753a3
Levin George <levgeo1_1@yahoo.com>
2016-05-23 10:24:54 +0100

    Views for the todo_items

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe "Viewing todo_items" do
   let!(:todo_list) {TodoList.create(title: "Grocery list", description: "Buy groceries")}
-
-  it "displays no items when todo_items is empty" do
+  before do
     visit "/todo_lists "
     within "#{todo_list.id}" do
       click_link "List Items"
     end
+  end
+
+  it "displays the title of the todo list" do
+    within("h1") do
+      expect(page).to have_content(todo_list.title)
+    end
+  end
+
+  it "displays no items when todo_items is empty" do
+
     expect(page).to have_content("TodoItems#Index")
   end
 end
